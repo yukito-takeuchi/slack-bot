862640c0d6088f6c684bd1ea62dcdfcbeb725eea
yukito-takeuchi <iyggf66974@yahoo.ne.jp>
2025-11-28 18:59:47 +0900

    ドメイン　追加OK

--- backend/scripts/init_data.py
+++ backend/scripts/init_data.py
@@ -140,6 +140,220 @@ def init_rss_sources():
             "name": "BASE",
             "url": "https://devblog.thebase.in/rss.xml"
         },
+
+        # スタートアップ・メガベンチャー系（15件）
+        {
+            "name": "SmartHR",
+            "url": "https://tech.smarthr.jp/feed"
+        },
+        {
+            "name": "Timee",
+            "url": "https://tech.timee.co.jp/feed"
+        },
+        {
+            "name": "READYFOR",
+            "url": "https://tech.readyfor.jp/feed"
+        },
+        {
+            "name": "10X",
+            "url": "https://product.10x.co.jp/feed"
+        },
+        {
+            "name": "ビズリーチ",
+            "url": "https://www.bizreach.co.jp/tech-blog/feed/"
+        },
+        {
+            "name": "Visional",
+            "url": "https://engineering.visional.inc/blog/feed"
+        },
+        {
+            "name": "プレイド",
+            "url": "https://tech.plaid.co.jp/feed"
+        },
+        {
+            "name": "ココナラ",
+            "url": "https://coconala-tech.com/feed/"
+        },
+        {
+            "name": "MICIN",
+            "url": "https://tech-blog.micin.jp/feed"
+        },
+        {
+            "name": "スタディサプリ",
+            "url": "https://blog.studysapuri.jp/feed"
+        },
+        {
+            "name": "Ateam",
+            "url": "https://www.a-tm.co.jp/top/blog/feed/"
+        },
+        {
+            "name": "アンドパッド",
+            "url": "https://tech.andpad.co.jp/feed"
+        },
+        {
+            "name": "ラクスル",
+            "url": "https://tech.raksul.com/feed"
+        },
+        {
+            "name": "カケハシ",
+            "url": "https://kakehashi-dev.hatenablog.com/rss"
+        },
+        {
+            "name": "Wantedly",
+            "url": "https://www.wantedly.com/companies/wantedly/feed"
+        },
+
+        # ゲーム会社系（8件）
+        {
+            "name": "Cygames",
+            "url": "https://tech.cygames.co.jp/feed/"
+        },
+        {
+            "name": "コロプラ",
+            "url": "https://colopl.dev/feed"
+        },
+        {
+            "name": "アカツキ",
+            "url": "https://hackerslab.aktsk.jp/feed"
+        },
+        {
+            "name": "QualiArts",
+            "url": "https://technote.qualiarts.jp/feed"
+        },
+        {
+            "name": "gumi",
+            "url": "https://developers.gu3.co.jp/feed"
+        },
+        {
+            "name": "KLab",
+            "url": "https://www.klab.com/jp/blog/tech/feed/"
+        },
+        {
+            "name": "セガ",
+            "url": "https://techblog.sega.jp/feed"
+        },
+        {
+            "name": "Craft Egg",
+            "url": "https://tech.craftegg.jp/feed"
+        },
+
+        # 決済・金融系（7件）
+        {
+            "name": "PayPay",
+            "url": "https://tech.paypay.ne.jp/rss.xml"
+        },
+        {
+            "name": "GMOインターネット",
+            "url": "https://recruit.gmo.jp/engineer/jisedai/blog/feed/"
+        },
+        {
+            "name": "GMOメディア",
+            "url": "https://techblog.gmo-media.jp/feed"
+        },
+        {
+            "name": "Kyash",
+            "url": "https://blog.kyash.co/feed"
+        },
+        {
+            "name": "bitFlyer",
+            "url": "https://blog.bitflyer.com/feed"
+        },
+        {
+            "name": "Coincheck",
+            "url": "https://tech.coincheck.blog/feed"
+        },
+        {
+            "name": "Moneytree",
+            "url": "https://getmoneytree.com/jp/blog/feed/"
+        },
+
+        # メディア・広告系（5件）
+        {
+            "name": "エムスリー",
+            "url": "https://www.m3tech.blog/feed"
+        },
+        {
+            "name": "オプト",
+            "url": "https://tech-blog.opt.ne.jp/feed"
+        },
+        {
+            "name": "サイボウズ",
+            "url": "https://blog.cybozu.io/feed"
+        },
+        {
+            "name": "pixiv",
+            "url": "https://inside.pixiv.blog/feed"
+        },
+        {
+            "name": "note",
+            "url": "https://note.com/notemag/m/m5b7aea49c9b1/rss"
+        },
+
+        # 大手IT・SIer系（5件）
+        {
+            "name": "NTTコミュニケーションズ",
+            "url": "https://engineers.ntt.com/feed"
+        },
+        {
+            "name": "NTTデータ",
+            "url": "https://www.nttdata.com/jp/ja/news/blog/feed/"
+        },
+        {
+            "name": "リクルートテクノロジーズ",
+            "url": "https://recruit-tech.co.jp/blog/feed/"
+        },
+        {
+            "name": "富士通",
+            "url": "https://www.fujitsu.com/jp/products/software/developer-tool/blog/feed/"
+        },
+        {
+            "name": "日立製作所",
+            "url": "https://www.hitachi.co.jp/rd/portal/feed/"
+        },
+
+        # EC・マーケットプレイス系（5件）
+        {
+            "name": "ヤプリ",
+            "url": "https://tech.yappli.io/feed"
+        },
+        {
+            "name": "オイシックス・ラ・大地",
+            "url": "https://creators.oisix.com/feed"
+        },
+        {
+            "name": "PKSHA Technology",
+            "url": "https://www.pkshatech.com/ja/blog/feed/"
+        },
+        {
+            "name": "ラクマ",
+            "url": "https://tech.rakus.co.jp/feed/"
+        },
+        {
+            "name": "Zホールディングス",
+            "url": "https://www.z-holdings.co.jp/ja/blog/feed/"
+        },
+
+        # その他有力企業（5件）
+        {
+            "name": "弁護士ドットコム",
+            "url": "https://creators.bengo4.com/feed"
+        },
+        {
+            "name": "Uzabase",
+            "url": "https://tech.uzabase.com/feed"
+        },
+        {
+            "name": "トレタ",
+            "url": "https://tech.toreta.in/feed"
+        },
+        {
+            "name": "フィードフォース",
+            "url": "https://developer.feedforce.jp/feed"
+        },
+        {
+            "name": "ペライチ",
+            "url": "https://peraichi.hatenablog.jp/rss"
+        },
     ]
 
     db = SessionLocal()
